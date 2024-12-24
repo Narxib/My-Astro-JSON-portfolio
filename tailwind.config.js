a86302d1a9b063b3ce4aed759f347b62ceb2986e
Brian Zaragoza Cerdá <brianzaragoza25@gmail.com>
2024-12-24 14:24:06 +0100

    day 2 progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textColor: "#6b7280",
+        titleColor: "#1f2937"
+      },
+      fontFamily: {
+        cour: ["Courier Prime", "serif"]
+      }
+    },
   },
   plugins: [],
 }
