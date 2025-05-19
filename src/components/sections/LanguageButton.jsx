3984270006ca530bc38636ee264f47040b86cb1a
Brian <brianzaragoza25@gmail.com>
2025-05-19 12:18:58 +0200

    Updated languages

--- src/components/sections/LanguageButton.jsx
+++ src/components/sections/LanguageButton.jsx
@@ -4,7 +4,10 @@ export default function LanguageButton() {
   const [lan, setLan] = useState("ES");
   const [visible, setVisible] = useState(false);
   return (
-    <div id="langButton" className="no-print flex-col w-auto relative h-16 rounded-lg  justify-center items-center text-center cursor-pointer self-end mx-20 my-[-40px]">
+    <div
+      id="langButton"
+      className="no-print flex-col w-auto relative h-16 rounded-lg  justify-center items-center text-center cursor-pointer self-end mx-20 my-[-40px]"
+    >
       <button
         className="bg-gray-300 text-gray-800 px-2 py-1 rounded-lg"
         onClick={() => setVisible(!visible)}
@@ -16,10 +19,16 @@ export default function LanguageButton() {
           visible ? "flex flex-col " : "hidden"
         } absolute top-9 p-2 left-[-9px] gap-2 border-dotted border-gray-400 border-[1px] justify-center items-center rounded-lg  w-14 h-auto`}
       >
-        <a className="w-full h-auto bg-white hover:bg-gray-300  rounded-md text-gray-800 " href="/en">
+        <a
+          className="w-full h-auto bg-white hover:bg-gray-300  rounded-md text-gray-800 "
+          href="/en"
+        >
           EN
         </a>
-        <a className="w-full h-auto rounded-md bg-white hover:bg-gray-300" href="/es">
+        <a
+          className="w-full h-auto rounded-md bg-white hover:bg-gray-300"
+          href="/"
+        >
           ES
         </a>
       </div>
